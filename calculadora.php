720bf8831f9e124e47b242e8db50d3b6d7419f54
Ifk1tiie <235318470+Ifk1tiie@users.noreply.github.com>
2026-08-27 10:07:04 -0500

    calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -10,11 +10,21 @@
 <form method="post">
 
     Número 1:
-    <input type="number" step="any" name="num1" required>
+    <input type="number" name="num1" step="any" required>
     <br><br>
 
     Número 2:
-    <input type="number" step="any" name="num2" required>
+    <input type="number" name="num2" step="any" required>
+    <br><br>
+
+    Operación:
+    <select name="operacion">
+        <option value="suma">Suma</option>
+        <option value="resta">Resta</option>
+        <option value="multiplicacion">Multiplicación</option>
+        <option value="redondeo">Redondeo</option>
+    </select>
+
     <br><br>
 
     <input type="submit" name="calcular" value="Calcular">
@@ -27,14 +37,28 @@ if (isset($_POST['calcular'])) {
 
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
+    $operacion = $_POST['operacion'];
 
-    echo "<h3>Resultados:</h3>";
+    if ($operacion == "suma") {
 
-    echo "Suma: " . ($num1 + $num2) . "<br>";
-    echo "Resta: " . ($num1 - $num2) . "<br>";
-    echo "Multiplicación: " . ($num1 * $num2) . "<br>";
+        $resultado = $num1 + $num2;
+        echo "Resultado de la suma: " . $resultado;
 
-    echo "Redondeo del primer número: " . round($num1) . "<br>";
+    } elseif ($operacion == "resta") {
+
+        $resultado = $num1 - $num2;
+        echo "Resultado de la resta: " . $resultado;
+
+    } elseif ($operacion == "multiplicacion") {
+
+        $resultado = $num1 * $num2;
+        echo "Resultado de la multiplicación: " . $resultado;
+
+    } elseif ($operacion == "redondeo") {
+
+        $resultado = round($num1);
+        echo "Resultado del redondeo: " . $resultado;
+    }
 }
 
 ?>
